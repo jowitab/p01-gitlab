9ca3f6812f80edbea750e206d1c34d0acb55d6cb
JowitaB <jowitabrodzik@gmail.com>
2020-06-03 18:06:46 +0200

    Czwarty commit

--- 01program.php
+++ 01program.php
@@ -1,5 +1,7 @@
 <?php
 echo ("Witaj");
 echo ("<br> Jestem programem 01");
-echo ("<br> Daje cos nowego od siebie");
+echo ("<br> Daje coś nowego od siebie, chociaż teraz nie jest już nowe");
+echo ("<br> Teraz to jest coś nowego");
+echo ("<br> Dalsza czesc programu");
 ?>
