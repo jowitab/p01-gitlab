d3f8efe738cd40cfe5dfc3f128c35d16babda84e
jowitab <66378617+jowitab@users.noreply.github.com>
2020-06-03 18:25:34 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -4,4 +4,5 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Daje coś nowego od siebie, chociaż teraz nie jest już nowe");
 echo ("<br> Teraz to jest coś nowego");
 echo ("<br> Dalsza czesc programu");
+echo ("<br> Tym razem nowe przez GitHub");
 ?>
